c7738ba2734fe70d3651a65e1d1deb7f264a18f4
josepozoz <jose.pozoz@ug.edu.ec>
2016-02-29 00:34:28 -0500

    Added files via upload

---
+++ ClusterAssignReducer.java
@@ -0,0 +1,33 @@
+package clusterassign;
+
+import java.io.IOException;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+import datapoint.DataPoint;
+
+/**
+  * Reducer class for the Cluster Assign step
+  */
+public class ClusterAssignReducer extends Reducer<DataPoint, DataPoint, DataPoint, DataPoint>
+{
+	/**
+	  * Overridden reduce method of the Reduce class
+	  * Parameters:	DataPoint key, a k-Means Centroid
+	  * 			Iterable<DataPoint> value, A list of Data Points associated to this Centriod
+	  *				Context context
+	  * Returns:	(key, value) pairs where
+	  *				key is a k-Means Centroid associated with the current Data Point
+	  *				value is one of the Data Points in the Iterable list
+	  * 
+	  * The function receives a (key, value) pair.
+	  * For every point in the Iterable list, it outputs the pair (k_Means Centroid, Data Point).
+	  */
+	@Override
+	public void reduce(DataPoint key, Iterable<DataPoint> values, Context context)
+		throws IOException, InterruptedException
+	{
+		for(DataPoint dataPoint : values)
+			context.write(key, dataPoint);
+	}
+}
