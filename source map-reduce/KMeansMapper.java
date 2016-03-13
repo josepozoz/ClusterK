a296034d32564c002df8e1aaf8644d5303afe8c6
josepozoz <jose.pozoz@ug.edu.ec>
2016-03-13 17:05:54 -0500

    Update KMeansMapper.java

--- source map-reduce/KMeansMapper.java
+++ source map-reduce/KMeansMapper.java
@@ -1,4 +1,4 @@
-package kmeans;
+package source map-reduce;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,40 +15,30 @@ import org.apache.hadoop.fs.Path;
 
 import datapoint.DataPoint;
 
-/**
-  * Mapper class for the k-Means Clustering.
-  */
+
 public class KMeansMapper extends Mapper<LongWritable, Text, DataPoint, DataPoint>
 {
-	/**
-	  * List to hold k-Means Centroids.
-	  */
+
 	ArrayList<DataPoint> kCentroids;
 
-	/**
-	  * Overridden setup method of Mapper class.
-	  * @param context
-	  * Reads the file containing k-Means Centroids, parses it and loads the Centroids into the ArrayList kCentroids.
-	  * Reads the file containing canopy centers, parses it and loads the Canopy Centers into the ArrayList canopyCenters.
-	  * Creates a HashMap (Canopy Center, List of Centroids in this canopy).
-	  */
+	
 	@Override
 	public void setup(Context context)
 		throws IOException, InterruptedException
 	{
-		// Call setup of super class
+	
 		super.setup(context);
 
-		// Allocate memory for kCentroids list
+		
 		kCentroids = new ArrayList<DataPoint>();
 
-		// Get the context's configuration
+		
 		Configuration configuration = context.getConfiguration();
 
-		// Get a handle of the HDFS
+		
 		FileSystem filesystem = FileSystem.get(configuration);
 		
-		// Read the k-Means Centroid File
+	
 		BufferedReader centroidReader = new BufferedReader(new InputStreamReader(filesystem.open(
 			new Path(configuration.get("fs.default.name") + configuration.get("kCentroidsFile")))));
 		String line = centroidReader.readLine();
@@ -68,30 +58,15 @@ public class KMeansMapper extends Mapper<LongWritable, Text, DataPoint, DataPoin
 		centroidReader.close();
 	}
 
-	/**
-	  * Overridden map function of Mapper Class
-	  * @param key
-	  * - an offset in the file
-	  * @param value
-	  * - A tab separated String for each DataPoint
-	  *	@param context
-	  * <b>Outputs:</b><br>
-	  * (key, value) pairs where,<br>
-	  *	key is a Cluster Centroid associated with the current Data Point<br>
-	  *	value is the Data Point being considered<br><br>
-	  * 
-	  * The function receives a (key, value) pair, parses it into the Data Point.
-	  * For each DataPoint, we copute its distance to each k-Means Centroid
-	  * The pair (K-Means Centroid, DataPoint) with the minimum distance is written as output.
-	  */
+	
 	@Override
 	public void map(LongWritable key, Text value, Context context)
 		throws IOException, InterruptedException
 	{
-		// Convert the value from Text to String 
+	
 		DataPoint dataPoint = new DataPoint(value.toString());
 
-		// Set the minimum distance to the maximum value a double can hold
+		
 		double minDistance = Double.MAX_VALUE;
 		int offset = -1;
 
